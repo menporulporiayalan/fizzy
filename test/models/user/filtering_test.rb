618b90c9259cfe7aa8475ae62776a04d3297ce4e
Dhishan <dhishan@dayspringlabs.com>
2026-09-16 11:13:03 +0530

    Scope filter users to board access list

--- test/models/user/filtering_test.rb
+++ test/models/user/filtering_test.rb
@@ -14,8 +14,32 @@ class User::FilteringTest < ActiveSupport::TestCase
     assert_equal [ "v1.2" ], filtering.releases
   end
 
+  test "users only offers people with access to the boards in play" do
+    assert_equal [ users(:david), users(:jz), users(:kevin) ], filtering.users
+  end
+
+  test "users narrows to the pinned board" do
+    boards(:private).accesses.create!(user: users(:david))
+
+    assert_equal [ users(:david), users(:kevin) ], filtering(board: boards(:private)).users
+  end
+
+  test "users narrows to the boards picked in the filter" do
+    boards(:private).accesses.create!(user: users(:david))
+
+    assert_equal [ users(:david), users(:kevin) ], filtering(filter: filter(board_ids: [ boards(:private).id ])).users
+  end
+
+  test "users keeps whoever is already picked, even without access to those boards" do
+    assert_includes filtering(filter: filter(creator_ids: [ users(:jason).id ])).users, users(:jason)
+  end
+
   private
-    def filtering
-      User::Filtering.new(users(:david), users(:david).filters.new)
+    def filtering(filter: filter(), board: nil)
+      User::Filtering.new(users(:david), filter, board: board)
+    end
+
+    def filter(**params)
+      users(:david).filters.new(**params)
     end
 end
